fbad341d2904be4891802e70bcc839817a25587e
smplcod <mplcod2021@gmail.com>
2024-05-19 22:02:43 +0600

    51:03

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,21 +1,20 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import styles from "./Home.module.css";
 import CarItem from "./car-item/Caritem.jsx";
-import { cars } from "./cars.data.js";
-// const cars = [];
+import { cars as carsData } from "./cars.data.js";
+import CreateCarForm from "./create-car-form/CreateCarForm.jsx";
 
 function Home() {
-  const [filter, setFilter] = useState("");
-  const filteredCars = useMemo(
-    () => cars.filter((car) => car.price > 20000),
-    [filter]
-  );
+  const [cars, setCars] = useState(carsData);
+  console.log(cars);
+
   return (
     <div>
       <h1>Cars catalog</h1>
+      <CreateCarForm setCars={setCars} />
       <div>
-        {filteredCars.length ? (
-          filteredCars.map((car) => <CarItem key={car.id} car={car} />)
+        {cars.length ? (
+          cars.map((car) => <CarItem key={car.id} car={car} />)
         ) : (
           <p>There are no cars</p>
         )}
---
+++ src/components/screens/home/create-car-form/CreateCarForm.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import styles from "./CreateCarForm.module.css";
+
+const CreateCarForm = ({ setCars }) => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+
+  const createCar = (e) => {
+    e.preventDefault();
+    setCars((prev) => [...prev, { id: prev.length + 1, name, price, image }]);
+  };
+
+  return (
+    <form className={styles.form}>
+      <input
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      <input
+        placeholder="Price"
+        onChange={(e) => setPrice(e.target.value)}
+        value={price}
+      />
+      <input
+        placeholder="Image"
+        onChange={(e) => setImage(e.target.value)}
+        value={image}
+      />
+      <button className="btn" onClick={(e) => createCar(e)}>
+        Create
+      </button>
+    </form>
+  );
+};
+
+export default CreateCarForm;
