2383e5c1794c349c259a6c8b0a0778d179fab481
smplcod <mplcod2021@gmail.com>
2024-05-19 23:49:22 +0600

    55:18 optimization of data

--- src/components/screens/home/create-car-form/CreateCarForm.jsx
+++ src/components/screens/home/create-car-form/CreateCarForm.jsx
@@ -1,36 +1,59 @@
 import React, { useState } from "react";
 import styles from "./CreateCarForm.module.css";
 
+const clearData = {
+  price: "",
+  name: "",
+  image: "",
+};
+
 const CreateCarForm = ({ setCars }) => {
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [image, setImage] = useState("");
+  const [data, setData] = useState({
+    price: "",
+    name: "",
+    image: "",
+  });
 
   const createCar = (e) => {
     e.preventDefault();
-    const priceNumber = parseFloat(price); // Конвертация строки в число
     setCars((prev) => [
+      { id: prev.length + 1, ...data, price: parseFloat(data.price) },
       ...prev,
-      { id: prev.length + 1, name, price: priceNumber, image },
     ]);
+    setData(clearData);
   };
 
   return (
     <form className={styles.form}>
       <input
         placeholder="Name"
-        onChange={(e) => setName(e.target.value)}
-        value={name}
+        onChange={(e) =>
+          setData((prev) => ({
+            ...prev,
+            name: e.target.value,
+          }))
+        }
+        value={data.name}
       />
       <input
         placeholder="Price"
-        onChange={(e) => setPrice(e.target.value)}
-        value={price}
+        onChange={(e) =>
+          setData((prev) => ({
+            ...prev,
+            price: e.target.value,
+          }))
+        }
+        value={data.price}
       />
       <input
         placeholder="Image"
-        onChange={(e) => setImage(e.target.value)}
-        value={image}
+        onChange={(e) =>
+          setData((prev) => ({
+            ...prev,
+            image: e.target.value,
+          }))
+        }
+        value={data.image}
       />
       <button className="btn" onClick={(e) => createCar(e)}>
         Create
